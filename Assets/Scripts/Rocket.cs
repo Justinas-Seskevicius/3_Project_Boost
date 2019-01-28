fe7af1c68c2b8ee88f4a55d358812c9bee35c7c9
zeFerret <justinas.seskevicius@gmail.com>
2019-01-28 22:41:08 +0200

    feat: thrust, rotation & sound

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    [SerializeField] float thrustSpeed = 25f;
+    [SerializeField] float rotationSpeed = 80f;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(new Vector2(0f, thrustSpeed));
+
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        audioSource.Stop();
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        rigidBody.freezeRotation = false;
+    }
+}
