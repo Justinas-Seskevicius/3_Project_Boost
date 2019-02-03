56a8a409073f69aaf39d8051d0fe1b65d7ae1b94
Justas-pc <justinas.seskevicius@gmail.com>
2019-02-03 17:42:35 +0200

    Death and Success sequence with sounds

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 
     [SerializeField] float thrustSpeed = 25f;
     [SerializeField] float rotationSpeed = 80f;
+    [SerializeField] float nextLevelDelay = 1f;
+
+    [SerializeField] AudioClip mainEngineSound;
+    [SerializeField] AudioClip deathExplosionSound;
+    [SerializeField] AudioClip victorySound;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
 
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,20 +29,18 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if(state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(new Vector2(0f, thrustSpeed));
-
-            if(!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else
         {
@@ -42,7 +48,17 @@ public class Rocket : MonoBehaviour
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(new Vector2(0f, thrustSpeed));
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngineSound);
+        }
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A))
@@ -58,14 +74,44 @@ public class Rocket : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(state != State.Alive) { return; }
+
         switch(collision.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log("Woop Woop");
+                break;
+            case "Finish":
+                InitiateSuccessSequance();
                 break;
             default:
-                Debug.Log(":(( you're dead");
+                InitiateDeathSequance();
                 break;
         }
+    }
+
+    private void InitiateSuccessSequance()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(victorySound);
+        state = State.Transcending;
+        Invoke("LoadNextLevel", nextLevelDelay);
+    }
+
+    private void InitiateDeathSequance()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathExplosionSound);
+        state = State.Dying;
+        Invoke("LoadFirstLevel", nextLevelDelay);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 }
